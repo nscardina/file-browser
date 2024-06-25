4dc7997fd57ce45836db1635c5e80d1e65b843c9
Noah Scardina <88603144+nscardina@users.noreply.github.com>
2024-06-25 16:47:28 -0400

    feat: Added beginnings of UI to main-view.fxml

--- src/main/java/scardina/file_browser/FileBrowserApplication.java
+++ src/main/java/scardina/file_browser/FileBrowserApplication.java
@@ -12,9 +12,13 @@ public class FileBrowserApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         var loader = new FXMLLoader(FileBrowserApplication.class.getResource("main-view.fxml"));
+
         var scene = new Scene(loader.load(), 640, 480);
+        scene.getStylesheets().add("scardina/file_browser/main-view.css");
+
         stage.setTitle("File Browser");
         stage.setScene(scene);
+
         stage.show();
     }
 
--- src/main/java/scardina/file_browser/FileBrowserMainController.java
+++ src/main/java/scardina/file_browser/FileBrowserMainController.java
@@ -1,6 +1,18 @@
 package scardina.file_browser;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+
 public class FileBrowserMainController {
 
 
+    public ListView pinnedFoldersListView;
+    public ListView favoritesListView;
+    public HBox fileViewingArea;
+
+    @FXML
+    protected void initialize() {
+
+    }
 }
